107a1ca1ec6df43d619da3acec425e28fec6a42b
frotylling58 <34075166+frotylling58@users.noreply.github.com>
2019-07-22 20:40:56 +0200

    Add files via upload

---
+++ src/test/java/it/unicam/cs/pa/dotsandboxes/manager/testController.java
@@ -0,0 +1,18 @@
+package it.unicam.cs.pa.dotsandboxes.manager;
+
+//import static org.junit.Assert.*;
+//import it.unicam.cs.pa.dotsandboxes.player.Bot;
+//import it.unicam.cs.pa.dotsandboxes.structure.Grid;
+
+import org.junit.Test;
+
+public class testController {
+
+	@Test()
+	public void testCreateController() {
+
+		// Bot bot = new Bot(new Grid(3), new DumbStrategy());
+		// assertEquals(3, bot.drawLine.toString(dot1, dot2));
+	}
+
+}
---
+++ src/test/java/it/unicam/cs/pa/dotsandboxes/player/testBot.java
@@ -0,0 +1,18 @@
+package it.unicam.cs.pa.dotsandboxes.player;
+
+import org.junit.Test;
+//import static org.junit.Assert.*;
+//import it.unicam.cs.pa.dotsandboxes.manager.DumbStrategy;
+//import it.unicam.cs.pa.dotsandboxes.player.Bot;
+//import it.unicam.cs.pa.dotsandboxes.structure.Grid;
+
+public class testBot {
+
+	@Test()
+	public void testCreateBot() {
+
+		// Bot bot = new Bot(new Grid(3), new DumbStrategy());
+		// assertEquals(3, bot.drawLine.suggestLine(dot, strategy));
+	}
+
+}
