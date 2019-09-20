d94084ffcb61557cf3e8df4eea9f8fd08dbb5f8c
frotylling58 <34075166+frotylling58@users.noreply.github.com>
2019-09-20 12:08:33 +0200

    Update AbstractPlayer.java

--- src/main/java/it/unicam/cs/pa/dotsandboxes/player/AbstractPlayer.java
+++ src/main/java/it/unicam/cs/pa/dotsandboxes/player/AbstractPlayer.java
@@ -1,10 +1,13 @@
 package it.unicam.cs.pa.dotsandboxes.player;
-
+/**
+* @author Donoval Candolfi - Mattia Incoronato
+*
+*/
 import it.unicam.cs.pa.dotsandboxes.structure.IGrid;
 
 /**
  * @param Questa classe ha lo scopo di definire il costruttore base per i player
- *
+ * @author Donoval Candolfi - Mattia Incoronato	
  */
 public abstract class AbstractPlayer implements Player {
 	protected IGrid grid = null;
