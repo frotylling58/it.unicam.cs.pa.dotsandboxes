02c8bf53bcadc451cfcf9c8a677a297fd9f81f05
frotylling58 <34075166+frotylling58@users.noreply.github.com>
2019-09-20 12:09:21 +0200

    Update Bot.java

--- src/main/java/it/unicam/cs/pa/dotsandboxes/player/Bot.java
+++ src/main/java/it/unicam/cs/pa/dotsandboxes/player/Bot.java
@@ -1,12 +1,15 @@
 package it.unicam.cs.pa.dotsandboxes.player;
-
+/**
+*@author Donoval Candolfi - Mattia Incoronato
+*
+*/
 import it.unicam.cs.pa.dotsandboxes.manager.Strategy;
 import it.unicam.cs.pa.dotsandboxes.structure.IGrid;
 import it.unicam.cs.pa.dotsandboxes.structure.Line;
 
 /**
  * @param
- *
+ * @author Donoval Candolfi - Mattia Incoronato
  */
 public class Bot extends AbstractPlayer {
 	private Strategy strategy = null;
