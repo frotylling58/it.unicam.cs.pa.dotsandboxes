d866a104dde18fe8ec261d5909339e2b99f820b4
frotylling58 <34075166+frotylling58@users.noreply.github.com>
2019-09-20 12:14:31 +0200

    Update testDot.java

--- src/test/java/it/unicam/cs/pa/dotsandboxes/structure/testDot.java
+++ src/test/java/it/unicam/cs/pa/dotsandboxes/structure/testDot.java
@@ -1,5 +1,8 @@
 package it.unicam.cs.pa.dotsandboxes.structure;
-
+/**
+* @author Donoval Candolfi - Mattia Incoronato
+*
+*/
 import org.junit.Test;
 
 import it.unicam.cs.pa.dotsandboxes.structure.Dot;
